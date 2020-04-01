8057f182750fa231c6117ac3186b25b9107e20d2
Parulian B <parulian.butarbutar@pintek.id>
2020-04-01 09:24:15 +0700

    fsf

--- schema/query/query.go
+++ schema/query/query.go
@@ -19,6 +19,16 @@ var RootQuery = graphql.NewObject(graphql.ObjectConfig{
 			},
 			Resolve: ProductResolve,
 		},
+		"Productssingle": &graphql.Field{
+			Type: graphql.NewList(types.ProductTypes),
+			//config param argument
+			Args: graphql.FieldConfigArgument{
+				"ID_PRO": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: ProductSingleResolve,
+		},
 		// untuk membuat object lainya tinggal di ulang
 
 	},
--- schema/query/resolverQuery.go
+++ schema/query/resolverQuery.go
@@ -15,7 +15,36 @@ func ProductResolve(param graphql.ResolveParams) (interface{}, error) {
 		panic(err.Error())
 	}
 	b = b[:0]
+
 	result, err := db.Query("select ID_PRO,PRO_NAME,QTE_IN_STOCK,ifnull(PRO_IMG,'') from Products")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for result.Next() {
+		err = result.Scan(&a.IdPro, &a.ProName, &a.QteStock, &a.ProImg)
+		if err != nil {
+			panic(err.Error())
+		}
+		b = append(b, a)
+
+	}
+
+	return b, nil
+}
+
+func ProductSingleResolve(param graphql.ResolveParams) (interface{}, error) {
+	var a types.Product
+	var b []types.Product
+	db, err := config.GetConnection()
+	if err != nil {
+		panic(err.Error())
+	}
+	b = b[:0]
+
+	id_pro, _ := params.Args["ID_PRO"].(string)
+
+	result, err := db.Query("select ID_PRO,PRO_NAME,QTE_IN_STOCK,ifnull(PRO_IMG,'') from Products where ID_PRO = ?", id_pro)
 	if err != nil {
 		panic(err.Error())
 	}
